0bb6362540c6c0f168943f1b43e4e8c358a9fa13
adrinava <a.navarredonda.2022@alumnos.urjc.es>
2025-11-13 21:35:16 +0100

    modificacion proceso entrenamiento YOLOv11

--- models/train.py
+++ models/train.py
@@ -1,16 +1,15 @@
 from ultralytics import YOLO
 
-# Modelo base pre-entrenado (tiny o small)
 model = YOLO("yolo11n.pt")  
 
 # Entrenamiento
 model.train(
-    data="data.v4i.yolov11/data.yaml",   # tu archivo con paths y clases
-    epochs=100,                 # puedes empezar con 50-100 y ajustar
-    imgsz=640,                  # tamaño de imagen para entrenamiento
-    batch=16,                   # tu GPU puede manejar más si quieres
-    device=0,                   # usa la RTX 4090
-    name="yolov11_cones"
+    data="data.v4i.yolov11/data.yaml", 
+    epochs=200,
+    imgsz=640,
+    batch=16,
+    device=0,
+    evolve=True
 )
 
 model.export(format="onnx")
